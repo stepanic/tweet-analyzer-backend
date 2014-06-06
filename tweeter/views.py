ecc011abe61996ef39ad0f114057c8f8793252ba
Matija Stepanic <stepanic.matija@gmail.com>
2014-06-06 04:33:33 +0200

    Finished JSON api template on route /analyzer

--- tweeter/views.py
+++ tweeter/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+import json
 
-# Create your views here.
+
+
+@csrf_exempt
+def analyzer(request):
+  response_data = {}
+
+  if request.method == 'GET':
+    response_data['status'] = False
+    response_data['message'] = 'GET is not allowed, only POST method!'
+
+
+  elif request.method == 'POST':
+    '''
+    Primanje podataka (izvora - Twitter usernamei i hashtagovi)
+    @dnevnikhr
+    #ladygaga
+    ...
+    njih N komada
+
+    i uz svaki source dolazi i podataka koliko se tvitova zeli za svakog dohvatiti
+
+    npr. na
+    sources[1] se nalazi '#ladygaga'
+    a na
+    how_many[1] se nalazi '12' sto znaci da se za #ladygaga u analizu ubacuje prvih 12 twitova
+    '''
+    sources =  request.POST.getlist("sources")
+    how_many =  request.POST.getlist("how_many")
+
+
+    '''
+    BZIK TODO
+    ovdje ubaciti spajanje na skripte za analizu
+
+    zamijeniti response_data['data'] koji su hardkodirani sa dobivenim podacima iz analizatora
+    zadrzati strukturu nazivlja atributa u dictionariju !!!
+
+    data je lista dictionarija koji u sebi imaju:
+    tweet_id
+    tweet_text
+    class_id
+
+    '''
+
+    response_data['status'] = True
+    response_data['message'] = 'Analyer finished successfully!'
+
+    '''
+    TODO zamijeniti
+    '''
+    response_data['data'] = [{'tweet_id':1, 'tweet_text': 'prvi 123', 'class_id': 3}, {'tweet_id':2, 'tweet_text': 'drugi 123', 'class_id':0}]
+
+
+  return HttpResponse(json.dumps(response_data), content_type="application/json")

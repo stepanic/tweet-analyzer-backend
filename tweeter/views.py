f00056d074dee78058e0a3c55e40c6afacfd8d24
Matija Stepanic <stepanic.matija@gmail.com>
2014-06-07 01:42:16 +0200

    ACAH try

--- tweeter/views.py
+++ tweeter/views.py
@@ -97,7 +97,7 @@ def analyzer(request):
   response["Access-Control-Allow-Origin"] = "*"
   response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
   response["Access-Control-Max-Age"] = "1000"
-  response["Access-Control-Allow-Headers"] = "*"
+  response["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
   return response
 
 
